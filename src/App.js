2bf90c6d9407e0201858920a601b02992b555371
Kdsingh333 <90763155+Kdsingh333@users.noreply.github.com>
2023-03-06 22:22:12 +0530

    engine start while loading

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import Boost from "./component/Boost";
 import Advanced from "./component/Advanced";
 import Showcase from "./component/Showcase";
 import Shortner from "./component/Shortener";
+import { useState } from 'react';
 
 
 
 function App() {
+  const [starter, setstarter] = useState("");
+
+  fetch("https://url-shortener-service-3t2m.onrender.com/")
+    .then((resp) => {
+      resp.json().then((result) => {
+        console.warn("result", result)
+        setstarter(result);
+
+
+      })
+    })
+
+  console.log(starter);
+
+
   return (
     <div>
-     
-      <Header/>
-      <Showcase/>
-      <Shortner/>
-      <Advanced/>
-      <Boost/>
-      <Footer/>
-      
+
+      <Header />
+      <Showcase />
+      <Shortner />
+      <Advanced />
+      <Boost />
+      <Footer />
+
     </div>
   );
 }
